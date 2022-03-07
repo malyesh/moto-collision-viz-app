b49719c499431740d5fa0c8b9185af075c15578b
malyesh <74512928+malyesh@users.noreply.github.com>
2022-03-07 16:07:09 -0500

    added map visual

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+// import React, { useRef, useEffect, useState } from 'react';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+
+mapboxgl.accessToken =
+  'pk.eyJ1Ijoic2VhbnlhcCIsImEiOiJja253cHNjeDUwdHNuMm9zN3JpdW5pMjh1In0.tPRrVItbczIscL75Q50Hfw';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lat: 40.785091,
+      lng: -73.968285,
+      zoom: 11,
+      pitch: 70,
+    };
+    this.mapContainer = React.createRef();
+  }
+  componentDidMount() {
+    const { lng, lat, zoom, pitch } = this.state;
+    const map = new mapboxgl.Map({
+      container: this.mapContainer.current,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [lng, lat],
+      zoom: zoom,
+      pitch: pitch,
+    });
+  }
+  render() {
+    return (
+      <div>
+        <div ref={this.mapContainer} className='map-container' />
+      </div>
+    );
+  }
 }
 
 export default App;
