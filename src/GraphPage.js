97618af8c908361e62d8273c23dd4f4fa1828acc
malyesh <74512928+malyesh@users.noreply.github.com>
2022-03-20 17:56:15 -0400

    fixed bugs

--- src/GraphPage.js
+++ src/GraphPage.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Navbar from './Navbar';
-import DataController from './DataController';
-import myData from './crashes2.json';
+// import DataController from './DataController';
+// import myData from './crashes2.json';
 // import { Link } from 'react-router';
 
 class GraphPage extends React.Component {
-  componentDidMount() {
-    console.log(myData);
-  }
+  //   componentDidMount() {
+  //     console.log(myData);
+  //   }
   render() {
     return (
       <div>
